8b801acf0a043a87c85e6cfa366487c8d3c1bd60
ShawonsGit <s.shawon0000@gmail.com>
2022-04-18 22:21:40 +0600

    about-edited

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,8 @@ const About = () => {
     return (
         <div>
             <div className='about-style'>
-            <h2>Name : Sharifuzzaman Shawon</h2>
+                <img className='w-50' src={`https://qph.cf2.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd`} alt="" />
+            <h5>Name : Sharifuzzaman Shawon</h5>
             <p>My name is shawon . I'm learning web full stack web developing . My goal is to keep learning more and more about web developing . I'am a college student and will be entering in university soon.  My target is to get a job so that I can pay my academics fee on my own . </p>
             </div>
         </div>
